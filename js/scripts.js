0376d44bb44a5b1b65f322e37709a20ffb33336b
Clear Organ <brian.mcintosh96@gmail.com>
2025-10-25 16:21:17 -0700

    Add format, comments for readability. Remove duplicate code blocks

--- js/scripts.js
+++ js/scripts.js
@@ -31,17 +31,6 @@ $(document).ready(function () {
       list.append(listItem);
       input.val("");
     }
-}
-// Add item to list when button is clicked
-$('#button').click(function (){
-    newItem();
-});
-
-//Add a line through item when clicked, delete upon double click
-list.on('click', 'li', function() {
-    $(this).toggleClass('strike');
-});
-
 
     myList.push(value);
     // store list in local storage
@@ -60,13 +49,13 @@ list.on('click', 'li', function() {
     $(this).remove();
   });
 
+  //Remove item from storage when deleted. And leave storage when marked off
 
+  //Now add a storage feature to save list items
 
-// Prevents form submission using enter key
-$('form[name="toDoList"]').submit(function(e) {
+  // Prevents form submission using enter key
+  $('form[name="toDoList"]').submit(function (e) {
     e.preventDefault();
     newItem();
-});
-
-
+  });
 });
