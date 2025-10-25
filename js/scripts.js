6e01eb6af3ed6f84378684da1d86469fae15c649
Clear Organ <brian.mcintosh96@gmail.com>
2025-10-25 16:09:06 -0700

    Add format and comments. Add jQuery DOM loader, no elements load before the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-$(document).ready(function (){
-var myList = [];
-let list = $('#list');
-let input = $('#input')
+// Wait until the DOM is fully loaded
+$(document).ready(function () {
+  // Initialize an empty array to hold list items
+  var myList = [];
+  // Retrieve stored list from local storage or initialize as empty array
+  const storedList = JSON.parse(localStorage.getItem("myList")) || [];
+  myList = storedList;
+  // Cache jQuery selectors for list and input elements
+  let list = $("#list");
+  let input = $("#input");
 
-// Function to add new item to list
-function newItem() {
+  $.each(storedList, function (key, value) {
+    let listItem = $(`<li class="list-item">${value} ${value}</li>`);
+    const deleteButton = $('<button class="listButton-delete">X</button>');
+    listItem.attr("draggable", "true");
+    listItem.append(deleteButton);
+    list.append(listItem);
+  });
+
+  //working on each function.
+
+  // Function to add new item to list
+  function newItem() {
     let value = input.val();
     let listItem = $(`<li class="list-item">${value}</li>`);
     listItem.append('<button class="listButton-delete">X</button>');
